4be3d18f898b3aef23bc01a8327b9b1fbd3a086a
Rachele Suy <rachelesuy@gmail.com>
2019-06-04 14:35:41 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,20 +1,21 @@
-CREATE TABLE projects(
-id INTEGER PRIMARY KEY,
-name TEXT,
-age INTEGER,
-title TEXT,
-category TEXT,
-funding_goal TEXT,
-start_date DATE,
-end_date DATE);
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date DATE,
+  end_date DATE
+);
 
-CREATE TABLE users(
-id INTEGER PRIMARY KEY,
-name TEXT,
-age INTEGER);
+ CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
 
-CREATE TABLE pledges(
-id INTEGER PRIMARY KEY,
-amount double,
-user_id INTEGER,
-project_id INTEGER);
+ CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
